6d6e61e3bb2a613e3ac0696cd19c7f6ded97f1ec
Mykola Peredrii <mykolaperedrii@proton.me>
2025-09-09 20:58:34 +0200

    nvim(nvim-highlight-colors): inital

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -34,6 +34,7 @@ vim.o.inccommand = "split"
 vim.o.cursorline = true
 vim.o.scrolloff = 10
 vim.o.confirm = true
+vim.opt.termguicolors = true
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("i", "jj", "<Esc>")
@@ -595,6 +596,53 @@ require("lazy").setup({
 			})
 
 			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+
+	{
+		"brenoprata10/nvim-highlight-colors",
+		config = function()
+			require("nvim-highlight-colors").setup({})
+
+			-- NOTE: https://github.com/brenoprata10/nvim-highlight-colors?tab=readme-ov-file#blinkcmp-integration
+			require("blink.cmp").setup({
+				completion = {
+					menu = {
+						draw = {
+							components = {
+								kind_icon = {
+									text = function(ctx)
+										local icon = ctx.kind_icon
+										if ctx.item.source_name == "LSP" then
+											local color_item = require("nvim-highlight-colors").format(
+												ctx.item.documentation,
+												{ kind = ctx.kind }
+											)
+											if color_item and color_item.abbr ~= "" then
+												icon = color_item.abbr
+											end
+										end
+										return icon .. ctx.icon_gap
+									end,
+									highlight = function(ctx)
+										local highlight = "BlinkCmpKind" .. ctx.kind
+										if ctx.item.source_name == "LSP" then
+											local color_item = require("nvim-highlight-colors").format(
+												ctx.item.documentation,
+												{ kind = ctx.kind }
+											)
+											if color_item and color_item.abbr_hl_group then
+												highlight = color_item.abbr_hl_group
+											end
+										end
+										return highlight
+									end,
+								},
+							},
+						},
+					},
+				},
+			})
 		end,
 	},
 
