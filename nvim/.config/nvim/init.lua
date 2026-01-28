94919f27645f48856d401c3caa954ca011f6f288
Mykola Peredrii <mykolaperedrii@proton.me>
2026-01-28 16:09:19 +0100

    nvim(multicursor.nvim): initial

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -312,6 +312,61 @@ require("lazy").setup({
             })
         end,
     },
+
+    {
+        "jake-stewart/multicursor.nvim",
+        branch = "1.0",
+        config = function()
+            -- stylua: ignore start
+
+            local mc = require("multicursor-nvim")
+            mc.setup()
+
+            local set = vim.keymap.set
+
+            set("n", "<M-k>", function() mc.lineAddCursor(-1) end, { desc = "Add cursor [k] up" })
+            set("n", "<M-i>", function() mc.lineSkipCursor(-1) end, { desc = "Sk[i]p cursor up" })
+            set("n", "<M-j>", function() mc.lineAddCursor(1) end, { desc = "Add cursor [j] down" })
+            set("n", "<M-u>", function() mc.lineSkipCursor(1) end, { desc = "Sk[u]p cursor down" })
+
+            set("n", "<M-n>", function() mc.matchAddCursor(1) end, { desc = "Match [n]ext" })
+            set("n", "<M-s>", function() mc.matchSkipCursor(1) end, { desc = "[s]kip match" })
+            set("n", "<M-p>", function() mc.matchAddCursor(-1) end, { desc = "Match [p]rev" })
+            set("n", "<M-a>", function() mc.matchSkipCursor(-1) end, { desc = "Skip b[a]ck" })
+
+            set("n", "<c-leftmouse>", mc.handleMouse)
+            set("n", "<c-leftdrag>", mc.handleMouseDrag)
+            set("n", "<c-leftrelease>", mc.handleMouseRelease)
+
+            set("n", "<c-q>", mc.toggleCursor)
+
+            mc.addKeymapLayer(function(layerSet)
+                layerSet({ "n", "x" }, "<left>", mc.prevCursor)
+                layerSet({ "n", "x" }, "<right>", mc.nextCursor)
+
+                layerSet({ "n", "x" }, "<leader>x", mc.deleteCursor)
+
+                layerSet("n", "<esc>", function()
+                    if not mc.cursorsEnabled() then
+                        mc.enableCursors()
+                    else
+                        mc.clearCursors()
+                    end
+                end)
+            end)
+
+            local hl = vim.api.nvim_set_hl
+            hl(0, "MultiCursorCursor", { reverse = true })
+            hl(0, "MultiCursorVisual", { link = "Visual" })
+            hl(0, "MultiCursorSign", { link = "SignColumn" })
+            hl(0, "MultiCursorMatchPreview", { link = "Search" })
+            hl(0, "MultiCursorDisabledCursor", { reverse = true })
+            hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+            hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
+
+            -- stylua: ignore end
+        end,
+    },
 })
 
 -- See `:help modeline`
